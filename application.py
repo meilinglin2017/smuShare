06c1e6bb008d888378a5f56aab12d69e85d4e376
TAN Jun Hui <junhui.tan.2017@smu.edu.sg>
2020-03-15 00:11:57 +0800

    added the 3 base files
    Wrote part of code to start off

---
+++ application.py
@@ -0,0 +1,55 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.debug = True
+
+# Change values when db is created
+username = ''
+password = ''
+host = ''
+port = 5432
+dbName = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{}:{}@{}:{}/{}'.format(username, password, host, port, dbName)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+### Import models ###
+
+### API Routes ###
+# User Methods
+@app.route("/searchFile/", methods = ['GET'])
+def searchFile():
+    pass
+
+@app.route("/getReviews/", methods = ['GET'])
+def getReviews():
+    pass
+
+@app.route("/uploadFile/", methods = ['POST'])
+def uploadFile():
+    pass
+
+@app.route("/uploadReview/", methods = ['POST'])
+def uploadReview():
+    pass
+
+# Developer Methods
+@app.route("/updateFile/", methods = ['PUT'])
+def updateFile():
+    pass
+
+@app.route("/deleteFile/", methods = ['DELETE'])
+def deleteFile():
+    pass
+
+@app.route("/deleteReview/", methods = ['DELETE'])
+def deleteReview():
+    pass
+
+### FrontEnd Routes ###
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,55 @@
+import datetime
+
+from application import db
+
+# File Info db table (Cannot use File cause is existing python object already)
+class Material(db.Model):
+    __tablename__ = 'file_info'
+
+    file_ID = db.Column(db.Integer, primary_key = True)
+    course_code = db.Column(db.String(10), nullable = False)
+    course_name = db.Column(db.String(200), nullable = False)
+    prof_name = db.Column(db.String(200), nullable = False)
+    course_term = db.Column(db.String(20), nullable = False)
+    file_name = db.Column(db.String(200), nullable = False)
+    rating_avg = db.Column(db.Float, nullable = False)
+    file_path = db.Column(db.String(200), nullable = False)
+    upload_date = db.Column(db.DateTime, default = datetime.datetime.utcnow)
+
+    reviews = db.relationship('Review', back_populates = 'upload_file', uselist = True, cascade = 'all, delete-orphan', lazy = True)
+
+    def __init__(self, course_code, course_name, prof_name, course_term, file_name, rating_avg, file_path, reviews = None):
+        self.course_code = course_code
+        self.course_name = course_name
+        self.prof_name = prof_name
+        self.course_term = course_term
+        self.file_name = file_name
+        self.rating_avg = rating_avg
+        self.file_path = file_path
+        self.reviews = [] if reviews is None else reviews
+
+    def __repr__(self):
+        return "{}_{}".format(self.file_name, self.file_ID)
+    
+    def serialize(self):
+        return {
+            'file_ID' : self.file_ID,
+            'course_code' : self.course_code,
+            'course_name' : self.course_name,
+            'prof_name' : self.prof_name,
+            'course_term' : self.course_term,
+            'file_name' : self.file_name,
+            'rating_avg' : self.rating_avg,
+            'file_path' : self.file_path,
+            'upload_date' : self.upload_date,
+            'reviews' : [{
+                'review_ID' : r.rating_ID,
+                'rating' : r.rating,
+                'review' : r.review,
+                'review_date' : r.review_date
+            } for r in self.reviews]
+        }
+
+# Review Info db table
+
+# User Info db table

2f9496d0d23c336a578e6d786a3538628fe62c39
TAN Jun Hui <junhui.tan.2017@smu.edu.sg>
2020-04-09 08:53:18 +0800

    aded prefix to s3bucket

--- s3bucket.py
+++ s3bucket.py
@@ -17,12 +17,12 @@ bucket_region = "us-west-2"
 bucket = s3_resource.Bucket(bucket_name)
 # S3 Helper Functions
 def s3_upload_file(file_name, file_content):
-    bucket.upload_fileobj(file_content, file_name, ExtraArgs={'ACL':'public-read'})
+    bucket.upload_fileobj(file_content, "uploads/" + file_name, ExtraArgs={'ACL':'public-read'})
 
 # Return the href to put into html for downloading
 def s3_get_link(file_name):
     # https://elasticbeanstalk-us-west-2-876671248402.s3-us-west-2.amazonaws.com/test.txt
-    return "https://{}.s3-{}.amazonaws.com/{}".format(bucket_name, bucket_region, file_name)
+    return "https://{}.s3-{}.amazonaws.com/{}".format(bucket_name, bucket_region, "uploads/" + file_name)
 
 def main():
     s3_upload_file("test.txt", open("test.txt", "rb"))
